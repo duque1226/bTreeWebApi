d448671f401111d77363a3acb39066a69d19b634
Juan Pablo Duque Ordoñez <duque93@hotmail.es>
2020-01-13 09:57:24 -0500

    Creando estructura inicial del proyecto

---
+++ binaryTreeWebAPI/ServicesCore/BTreeServiceCore.cs
@@ -0,0 +1,22 @@
+﻿using binaryTreeWebAPI.Interfaces;
+using binaryTreeWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace binaryTreeWebAPI.ServicesCore
+{
+    public class BTreeServiceCore : IbTree
+    {
+        public int calculateLCA(ref Node actualNode, int value1, int value2, ref Node originNode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BTree createBTree(string bTreeName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
